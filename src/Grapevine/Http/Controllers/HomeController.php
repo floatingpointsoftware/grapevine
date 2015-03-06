d42e2a6ad4b0e52d6acc7d5034d3737c620b0b4f
Mike Dugan <mike@mjdugan.com>
2015-03-05 19:35:05 -0500

    conditional button on home page

--- src/Grapevine/Http/Controllers/HomeController.php
+++ src/Grapevine/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 namespace FloatingPoint\Grapevine\Http\Controllers;
 
 use FloatingPoint\Grapevine\Library\Support\Controller;
+use FloatingPoint\Grapevine\Modules\Categories\Data\CategoryRepository;
 use FloatingPoint\Grapevine\Modules\Discussions\Data\DiscussionRepository;
 
 class HomeController extends Controller
@@ -12,11 +13,17 @@ class HomeController extends Controller
     private $discussions;
 
     /**
+     * @var CategoryRepository
+     */
+    private $categories;
+
+    /**
      * @param DiscussionRepository $discussions
      */
-    function __construct(DiscussionRepository $discussions)
+    function __construct(DiscussionRepository $discussions, CategoryRepository $categories)
     {
         $this->discussions = $discussions;
+        $this->categories = $categories;
     }
 
     /**
@@ -27,7 +34,9 @@ class HomeController extends Controller
     public function index()
     {
         $discussions = $this->discussions->getRecent();
+        $categoryCount = $this->categories->count();
 
-        return $this->respond('home.index', compact('discussions'));
+
+        return $this->respond('home.index', compact('discussions', 'categoryCount'));
     }
 }
